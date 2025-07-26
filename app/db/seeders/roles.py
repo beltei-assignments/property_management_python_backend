0dcfb8f225ee4c7cc13f82a0e2db437f5b0a9645
chetracoding <hongchetra12@gmail.com>
2025-07-26 18:55:24 +0700

    chore(router): add prefix router

--- app/db/seeders/roles.py
+++ app/db/seeders/roles.py
@@ -4,15 +4,15 @@ from app.models.role_model import Role
 
 def seed_roles():
     db = SessionLocal()
-    role = [
+    roles = [
         {"name": "Admin", "value": "admin"},
         {"name": "Manager", "value": "manager"},
         {"name": "Buyer", "value": "buyer"},
         {"name": "Seller", "value": "seller"},
     ]
     try:
-        for r in role:
-            db.add(Role(**r))
+        for role in roles:
+            db.add(Role(**role))
         db.commit()
     finally:
         db.close()
